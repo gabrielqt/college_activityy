c230af5e62b213255081df69791c3126167648a9
Gabriel Traiano <gabriel.traiano4@gmail.com>
2024-06-11 19:28:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ number = randomnumbers()
 for i in range(1,11):
     
     game = randomgame(number=number)
+    
     if game == True:
+        print('Você ganhou :D')
+        break
+    if i == 10:
+        print(f'Você perdeu :( o número era {number}')
         break
     print(verify(randomnumber=number, usernumber=game))
